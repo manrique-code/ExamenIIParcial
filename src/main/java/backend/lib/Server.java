306de4b21804350e7e1e9fe003bdabe0a70dad85
manrique-code <sergio.mnrq1@gmail.com>
2020-11-27 13:40:59 -0600

    1. Instancia del server creada 2. Añadido algunos archivos a ignorarse.

--- src/main/java/backend/lib/Server.java
+++ src/main/java/backend/lib/Server.java
@@ -1,7 +1,32 @@
 package backend.lib;
 
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(4000), 0);
+
+            // request para ver el nombre y la versión de la app
+            server.createContext("/", (httpExchange) -> {
+                JSONObject jres = new JSONObject();
+
+                jres.put("Nombre", "");
+            });
+
+            server.setExecutor(null);
+            server.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
